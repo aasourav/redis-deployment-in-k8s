8ce538fa2f0d3d0e66e52be5fbdb9a2fb467a8ea
Ahsan Sourav <aasourav@yahoo.com>
2024-09-08 14:54:20 +0600

    redis deployment

---
+++ README.md
@@ -0,0 +1,8 @@
+for redis deployment we used [Ot-redis-operator](https://ot-redis-operator.netlify.app/) Operator
+
+[Operator Source Code](https://github.com/OT-CONTAINER-KIT/redis-operator/tree/master)
+
+This operator have multiple architecture
+    * Replication
+    * Standalone
+    * Sentinel
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// Create a Redis client
+var ctx = context.Background()
+
+func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379", // Redis server address
+		Password: "password",       // No password set
+	})
+
+	// Ping Redis to check the connection
+	pong, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		log.Fatalf("Could not connect to Redis: %v", err)
+	}
+	fmt.Printf("Redis Ping: %s\n", pong)
+
+	// Set a key-value pair in Redis
+	err = rdb.Set(ctx, "mykey", "myvalue", 0).Err()
+	if err != nil {
+		log.Fatalf("Could not set key: %v", err)
+	}
+
+	// Get the value of the key from Redis
+	val, err := rdb.Get(ctx, "mykey").Result()
+	if err != nil {
+		log.Fatalf("Could not get key: %v", err)
+	}
+	fmt.Printf("mykey: %s\n", val)
+
+	// Test getting a non-existent key
+	val2, err := rdb.Get(ctx, "nonexistent").Result()
+	if err == redis.Nil {
+		fmt.Println("Key does not exist")
+	} else if err != nil {
+		log.Fatalf("Error getting key: %v", err)
+	} else {
+		fmt.Printf("nonexistent: %s\n", val2)
+	}
+}
